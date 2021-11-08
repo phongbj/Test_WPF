6ccb6826316d3e061aeb29ac87cf6f936658c798
phongbj <20520364@gm.uit.edu.vn>
2021-11-08 16:21:40 +0700

    Add project files.

---
+++ Test_WPF/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test_WPF
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        ListView listView = new ListView();
+        private void timkiem_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(timkiem.Text) == false)
+            {
+
+            }
+        }
+    } 
+}
---
+++ Test_WPF/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Test_WPF.ViewModel
+{
+    public class MainViewModel:BaseViewModel
+    {
+        public MainViewModel()
+        {
+          
+        }
+    }
+}
